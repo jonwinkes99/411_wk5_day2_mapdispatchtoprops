da0e3a6128c2342c7cb1214cd843159c29eabb1e
winkleman <jwinks18@gmail.com>
2020-03-15 13:34:37 -0500

    kast

---
+++ src/redux/Actions.js
@@ -0,0 +1,17 @@
+const addCar = (car) => {
+    
+    return {
+        type: 'ADD_CAR',
+        value: car
+    }
+}
+
+const removeCar = (index) => {
+    
+    return {
+        type: 'REMOVE_CAR',
+        value: index
+    }
+}
+
+export {addCar, removeCar}
